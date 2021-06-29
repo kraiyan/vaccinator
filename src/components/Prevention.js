4428ad401151edb1df37ef70a5f439f0f114665f
sumitkk12 <69508514+sumitkk12@users.noreply.github.com>
2021-06-29 20:17:25 +0530

    added content

--- src/components/Prevention.js
+++ src/components/Prevention.js
@@ -1,17 +1,109 @@
-import React from 'react'
-import Main from "./main.js"
+import Sanitizer from "../images/sanitizer.jpg"
+import Coughpic from "../images/coughpic.jpg"
+import Mask from "../images/mask.jpg"
+import Fever from "../images/fever.jpg"
+import Cover from "../images/cover.jpg"
+import { Card } from "react-bootstrap"
+import Safe from "../images/safe.jpg"
 
- const Prevention= ()=> {
+
+const Prevention = () => {
     return (
-          <>
-          <Main/>
-        <div>
-              
-            <h1>Prevention Page</h1>
-        </div>
+
+        <>
+
+
+            <div className="row">
+                <Card style={{ width: '18rem', marginLeft: "50px" }}>
+                    <Card.Img variant="top" src={Sanitizer} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b> Clean your hands often. Use soap and water, or an alcohol-based hand rub.</b>
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
+
+
+
+
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={Mask} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b>Wear a mask when physical distancing is not possible.
+                                Don’t touch your eyes, nose or mouth.</b>
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={Coughpic} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b>Maintain a safe distance from anyone who is coughing or sneezing.</b>
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
+
+
+
+
+
+
+            </div>
+            <br /><br />
+
+            <div className="row">
+
+                <Card style={{ width: '18rem', marginLeft: "50px" }}>
+                    <Card.Img variant="top" src={Fever} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b>If you have a fever, cough and difficulty breathing, seek medical attention.</b>
+                        </Card.Text>
+
+                    </Card.Body>
+                </Card>
+
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={Cover} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b>Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.</b>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={Safe} />
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <b>
+                                Stay home if you feel unwell.</b>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+
+
+
+
+
+
 
         </>
     )
 }
-
-export default Prevention
+export default Prevention;
